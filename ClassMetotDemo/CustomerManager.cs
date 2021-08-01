263b1897f783d107373296d7a70f27d2c1a38b7e
Yalçın Uzun <yalcinuzun52@gmail.com>
2021-08-01 23:18:21 +0300

    Tüm istekler yerine getirildi.
    Projeniz şunu yapacak.
    
    Bir bankada müşteri takibi yapmak istiyorsunuz.
    Musteri isimli bir Class oluşturunuz. Müşteriye istediğiniz özellikleri ekleyiniz. (Id,Ad,Soyad....)
    MusteriManager sınıfı oluşturunuz. Musteri parametresi alarak Musteri ekleme, listeleme,silme gibi metotları simule ediniz.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        List<Customer> customers = new List<Customer> { };
+        public void Add(Customer customer)
+        {
+            customers.Add(customer);
+            Console.WriteLine("Müşteri başarıyla eklendi: " + customer.FirstName + " " + customer.LastName);
+        }
+
+        public void Remove(Customer customer)
+        {
+            customers.Remove(customer);
+            Console.WriteLine("Müşteri başarıyla silindi." + customer.FirstName + " " + customer.LastName);
+        }
+
+        public void List()
+        {
+            foreach (var customer in customers)
+            {
+                Console.WriteLine("Id: " + customer.Id);
+                Console.WriteLine("İsim: " + customer.FirstName);
+                Console.WriteLine("Soyisim: " + customer.LastName);
+                Console.WriteLine("********");
+            }
+
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.Id = 1;
+            customer1.FirstName = "Yalçın";
+            customer1.LastName = "Uzun";
+
+            Customer customer2 = new Customer();
+            customer2.Id = 2;
+            customer2.FirstName = "Engin";
+            customer2.LastName = "Demiroğ";
+
+            Customer customer3 = new Customer();
+            customer3.Id = 3;
+            customer3.FirstName = "Kemalcan";
+            customer3.LastName = "Parlak";
+
+            Customer customer4 = new Customer();
+            customer4.Id = 4;
+            customer4.FirstName = "Dora";
+            customer4.LastName = "Özsoy";
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(customer1);
+            customerManager.Add(customer2);
+            customerManager.Add(customer3);
+            customerManager.Add(customer4);
+            Console.WriteLine("------------ Müşteriler Eklendi ------------");
+            Console.WriteLine(" ");
+
+            customerManager.List();
+            Console.WriteLine("----------- Müşteriler Listlendi -----------");
+            Console.WriteLine(" ");
+
+
+            customerManager.Remove(customer3);
+            customerManager.Remove(customer1);
+            Console.WriteLine("------------ Müşteriler Silindi ------------");
+            Console.WriteLine(" ");
+
+
+            customerManager.List();
+            Console.WriteLine("----------- Müşteriler Listlendi -----------");
+            Console.WriteLine(" ");
+        }
+    }
+}
